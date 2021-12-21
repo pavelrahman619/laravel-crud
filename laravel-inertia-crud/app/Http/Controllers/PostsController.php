a68c94d591f7f4df3ac68bc238361693ce87b89c
pavelrahman619 <96363207+pavelrahman619@users.noreply.github.com>
2021-12-21 11:27:58 +0600

    stable v1.0

--- laravel-inertia-crud/app/Http/Controllers/PostsController.php
+++ laravel-inertia-crud/app/Http/Controllers/PostsController.php
@@ -28,7 +28,22 @@ class PostsController extends Controller
 
     public function store(Request $request)
     {
-        
+        $this->getValidate($request);
+
+        $post = new Post();
+
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+
+        if($request->file('image')) {
+            $post->image = $this->upload($request);
+        }
+
+        $post->save();
+
+        $request->session()->flash('success', 'Post created successfully!');
+
+        return redirect()->route('post.index');
     }
 
     public function edit($id)
@@ -40,11 +55,55 @@ class PostsController extends Controller
 
     public function update(Request $request, $id)
     {
-        
+        $this->getValidate($request, $id);
+
+        $post = Post::find($id);
+
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+
+        if($request->file('image')) {
+            $post->image = $this->upload($request);
+        }
+
+        $post->save();
+
+        $request->session()->flash('success', 'Post updated successfully!');
+
+        return redirect()->route('post.index');
     }
 
     public function destroy(Request $request, $id)
     {
-        
+        Post::find($id)->delete();
+
+        $request->session()->flash('success', 'Post deleted successfully!');
+
+        return redirect()->route('post.index');
+    }
+
+    /**
+     * @param Request $request
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    private function getValidate(Request $request, $id = null): void
+    {
+        $data = [
+            'title' => 'required',
+            'content' => 'required'
+        ];
+
+        $this->validate($request, $data);
+    }
+
+    private function upload($request)
+    {
+        $image = $request->file('image');
+
+        $imageName = md5(uniqid()) . "." . $image->getClientOriginalExtension();
+
+        $image->move(public_path('uploads'), $imageName);
+
+        return $imageName;
     }
 }
